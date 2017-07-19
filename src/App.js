06d0c834980f27615a74e6c4d7c13ed7091aa6d7
Art Gillespie <art@turbinelabs.io>
2017-07-19 12:49:44 -0700

    Add ComponentOne

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import ComponentOne from './ComponentOne'
 
 class App extends Component {
   render() {
@@ -10,12 +11,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ComponentOne />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
